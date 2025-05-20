0a9d6c8571c40b8e834eb70a06084df5f33b541c
Jeremy Le <jeremyle420@gmail.com>
2025-05-20 16:09:12 +1000

    feat(MVP): set up framework

---
+++ code/introduction/codeforces_template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+/* ============================================ */
+
+void solve() {
+    int n;
+    cin >> n;
+}
+
+/* ============================================ */
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
